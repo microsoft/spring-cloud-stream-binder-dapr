ef7f2a640c2b8d0cd84840346b7d08978ef0bb0c
MouMangTai <1421311452@qq.com>
2022-06-28 10:03:57 +0800

    Modify the life cycle of daprStub

--- src/main/java/com/azure/spring/cloud/stream/binder/dapr/DaprMessageChannelBinder.java
+++ src/main/java/com/azure/spring/cloud/stream/binder/dapr/DaprMessageChannelBinder.java
@@ -3,6 +3,8 @@
 
 package com.azure.spring.cloud.stream.binder.dapr;
 
+import com.azure.spring.cloud.stream.binder.dapr.impl.DaprMessageHandler;
+import com.azure.spring.cloud.stream.binder.dapr.impl.DaprMessageProducer;
 import com.azure.spring.cloud.stream.binder.dapr.properties.DaprConsumerProperties;
 import com.azure.spring.cloud.stream.binder.dapr.properties.DaprExtendedBindingProperties;
 import com.azure.spring.cloud.stream.binder.dapr.properties.DaprProducerProperties;
@@ -21,6 +23,7 @@ import org.springframework.cloud.stream.provisioning.ProducerDestination;
 import org.springframework.integration.core.MessageProducer;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
+import org.springframework.util.Assert;
 
 /**
  * {@link DaprMessageChannelBinder} extend the AbstractMessageChannelBinder class.
@@ -30,11 +33,15 @@ public class DaprMessageChannelBinder extends
 		AbstractMessageChannelBinder<ExtendedConsumerProperties<DaprConsumerProperties>, ExtendedProducerProperties<DaprProducerProperties>, DaprBinderProvisioner>
 		implements
 		ExtendedPropertiesBinder<MessageChannel, DaprConsumerProperties, DaprProducerProperties> {
+
+	private DaprGrpc.DaprStub daprStub;
+
 	private DaprExtendedBindingProperties bindingProperties = new DaprExtendedBindingProperties();
+
 	/**
 	 * Construct a {@link DaprMessageChannelBinder} with the specified headers to embed and {@link DaprBinderProvisioner}.
 	 *
-	 * @param headersToEmbed the headers to enbed
+	 * @param headersToEmbed the headers to embed
 	 * @param provisioningProvider the provisioning provider
 	 */
 	public DaprMessageChannelBinder(String[] headersToEmbed,
@@ -43,18 +50,26 @@ public class DaprMessageChannelBinder extends
 	}
 
 	@Override
-	protected MessageHandler createProducerMessageHandler(ProducerDestination destination, ExtendedProducerProperties<DaprProducerProperties> producerProperties, MessageChannel errorChannel) throws Exception {
-		ManagedChannel managedChannel = ManagedChannelBuilder.forAddress(
-				producerProperties.getExtension().getSidecarIp(),
-				producerProperties.getExtension().getGrpcPort()).usePlaintext().build();
-		DaprGrpc.DaprFutureStub futureStub = DaprGrpc.newFutureStub(managedChannel);
-		DaprMessageHandler daprMessageHandler = new DaprMessageHandler(futureStub, destination.getName());
-		daprMessageHandler.setPubsubName(producerProperties.getExtension().getPubsubName());
+	protected MessageHandler createProducerMessageHandler(ProducerDestination destination, ExtendedProducerProperties<DaprProducerProperties> producerProperties, MessageChannel errorChannel) {
+		Assert.notNull(getDaprStub(producerProperties.getExtension().getSidecarIp(), producerProperties.getExtension().getGrpcPort()), "daprStub can't be null when create a producer");
+		DaprMessageHandler daprMessageHandler = new DaprMessageHandler(destination.getName(),
+				producerProperties.getExtension().getPubsubName(), this.daprStub);
+		daprMessageHandler.setSync(producerProperties.getExtension().isSync());
+		daprMessageHandler.setSendTimeout(producerProperties.getExtension().getSendTimeout().toMillis());
+		daprMessageHandler.setBeanFactory(getBeanFactory());
 		return daprMessageHandler;
 	}
 
+	private DaprGrpc.DaprStub getDaprStub(String sidecarIp, Integer grpcPort) {
+		if (this.daprStub == null) {
+			ManagedChannel managedChannel = ManagedChannelBuilder.forAddress(sidecarIp, grpcPort).usePlaintext().build();
+			this.daprStub = DaprGrpc.newStub(managedChannel);
+		}
+		return this.daprStub;
+	}
+
 	@Override
-	protected MessageProducer createConsumerEndpoint(ConsumerDestination destination, String group, ExtendedConsumerProperties<DaprConsumerProperties> consumerProperties) throws Exception {
+	protected MessageProducer createConsumerEndpoint(ConsumerDestination destination, String group, ExtendedConsumerProperties<DaprConsumerProperties> consumerProperties) {
 		return new DaprMessageProducer();
 	}
 
@@ -78,10 +93,20 @@ public class DaprMessageChannelBinder extends
 		return this.bindingProperties.getExtendedPropertiesEntryClass();
 	}
 
+	/**
+	 * Get bindingProperties.
+	 *
+	 * @return bindingProperties the bindingProperties
+	 */
 	public DaprExtendedBindingProperties getBindingProperties() {
 		return bindingProperties;
 	}
 
+	/**
+	 * Set bindingProperties.
+	 *
+	 * @param bindingProperties the bindingProperties
+	 */
 	public void setBindingProperties(DaprExtendedBindingProperties bindingProperties) {
 		this.bindingProperties = bindingProperties;
 	}
